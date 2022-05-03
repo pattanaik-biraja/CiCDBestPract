68a73f76bbfe54c0d09037a79d2c2f5190d22178
Biraj <automation.biraja@gmail.com>
2022-05-03 12:35:56 +0530

    initial-projrelated-changes

---
+++ src/main/java/com/dev/CiCDprojApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCDprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCDprojApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dev/controller/CiCdBootController.java
@@ -0,0 +1,18 @@
+package com.dev.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class CiCdBootController {
+	
+	
+	@GetMapping("/{name}")
+    public ResponseEntity<String> askQuestion(@PathVariable(value = "name") String name) {
+      return ResponseEntity.ok("Hey " + name + ", Welcome to planet Earth..");
+    }
+}
