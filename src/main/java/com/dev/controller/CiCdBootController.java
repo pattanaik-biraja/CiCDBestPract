d29b15a89f2c93a8b21146809c82cfefc9e8cbb9
Biraj <automation.biraja@gmail.com>
2022-05-09 00:33:15 +0530

    ci related changes

--- src/main/java/com/dev/controller/CiCdBootController.java
+++ src/main/java/com/dev/controller/CiCdBootController.java
@@ -13,6 +13,6 @@ public class CiCdBootController {
 	
 	@GetMapping("/{name}")
     public ResponseEntity<String> askQuestion(@PathVariable(value = "name") String name) {
-      return ResponseEntity.ok("Hey " + name + ", Welcome to planet Earth..");
+      return ResponseEntity.ok("Hey " + name + ", Welcome to CI pipe line..");
     }
 }
