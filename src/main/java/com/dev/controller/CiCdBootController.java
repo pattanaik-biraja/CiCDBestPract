5f5c21fd9b1c84bb379a9f496815ed9497811d26
Biraj <automation.biraja@gmail.com>
2022-05-09 00:44:29 +0530

    cicd related changes-maynottrigger webhook

--- src/main/java/com/dev/controller/CiCdBootController.java
+++ src/main/java/com/dev/controller/CiCdBootController.java
@@ -13,6 +13,6 @@ public class CiCdBootController {
 	
 	@GetMapping("/{name}")
     public ResponseEntity<String> askQuestion(@PathVariable(value = "name") String name) {
-      return ResponseEntity.ok("Hey " + name + ", Welcome to CICD pipe line through localhost jenkins and github..");
+      return ResponseEntity.ok("Hey " + name + ", Welcome to CICD pipe line through jenkins running at localhost and github..");
     }
 }
