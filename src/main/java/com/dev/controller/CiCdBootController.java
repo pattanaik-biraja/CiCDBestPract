860e71264127bef878f51174c44c408eb5ebacda
Biraj <automation.biraja@gmail.com>
2022-05-09 00:37:51 +0530

    cicd related changes

--- src/main/java/com/dev/controller/CiCdBootController.java
+++ src/main/java/com/dev/controller/CiCdBootController.java
@@ -13,6 +13,6 @@ public class CiCdBootController {
 	
 	@GetMapping("/{name}")
     public ResponseEntity<String> askQuestion(@PathVariable(value = "name") String name) {
-      return ResponseEntity.ok("Hey " + name + ", Welcome to CI pipe line..");
+      return ResponseEntity.ok("Hey " + name + ", Welcome to CICD pipe line through localhost jenkins and github..");
     }
 }
